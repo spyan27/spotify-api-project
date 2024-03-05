79149fbd58cc97e1454bdd685f6e9db605a04820
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-03-05 16:34:57 -0500

    added search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SpotifyData from './SpotifyData';
+import './style.css';
 
 function App() {
   return (
     <div>
-      <h1>MySpotify</h1>
+      <h1 className= "text-align: center" >MySpotify</h1>
       <SpotifyData />
     </div>
   );
--- client/src/SpotifyData.js
+++ client/src/SpotifyData.js
@@ -1,30 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './style.css';
 
 const SpotifyData = () => {
   const [artistData, setArtistData] = useState(null);
+  const [artistName, setArtistName] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/get-spotify-data?artistName=${artistName}`);
+      setArtistData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/get-spotify-data');
-        setArtistData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
     fetchData();
-  }, []);
+  }, [artistName]);
+
+  const handleSearch = () => {
+    fetchData();
+  };
 
   if (!artistData) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div>
+    <div className="container">
       <h1>{artistData.name}</h1>
-      <p>Followers: {artistData.followers}</p>
+      <p style={{ color: 'blue' }}> Followers: {artistData.followers}</p>
+
+      <div>
+        <label htmlFor="artistSearch">Enter Artist Name:</label>
+        <input
+          type="text"
+          id="artistSearch"
+          placeholder="Search for an artist..."
+          value={artistName}
+          onChange={(e) => setArtistName(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
 
       <h2>Top Tracks:</h2>
       <ul>
